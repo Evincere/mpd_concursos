3ca8a6cb1dfaa2c6cbb25a245f950aa053a9e158
Semper <elsemper@gmail.com>
2025-02-15 16:07:55 -0300

    inscripciones funcionando ok

--- concurso-backend/src/main/java/ar/gov/mpd/concursobackend/shared/infrastructure/security/SecurityUtils.java
+++ concurso-backend/src/main/java/ar/gov/mpd/concursobackend/shared/infrastructure/security/SecurityUtils.java
@@ -1,7 +1,5 @@
 package ar.gov.mpd.concursobackend.shared.infrastructure.security;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import ar.gov.mpd.concursobackend.shared.application.service.SecurityService;
